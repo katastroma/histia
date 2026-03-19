854e96e0d24e302758121f0e34aec38d963632e6
Nathan Blair <me@nathanblair.rocks>
2026-03-19 14:19:31 -0400

    chore: add initial work on what histia will need to care about

---
+++ internal/const.go
@@ -0,0 +1,7 @@
+//revive:disable:package-comments
+package internal
+
+const (
+// Finalizer is the finalizer string histia sets on Applications
+// Finalizer = katastromaorgv0.Group + "/cleanup"
+)
